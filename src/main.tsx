aca1539063af9f7baab1bdf5c71d5a35a5b41dd3
ben8622 <benjaminjknight@gmail.com>
2025-11-14 07:16:54 -0600

    got pages with react router working and a nasty nav bar

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Blogs from './pages/Blogs.tsx'
+import Home from './pages/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/pages/Blogs.tsx
@@ -0,0 +1,10 @@
+function Blogs() {
+    return (
+        <div>
+            <h1>Blogs Page</h1>
+            <p>Welcome to the Blogs Page!</p>
+        </div>
+    )
+}
+
+export default Blogs
