b97c1a15a6cf7a556b42da726a25428d422901af
ben8622 <benjaminjknight@gmail.com>
2025-11-15 13:56:45 -0600

    nav bar tidied up, also added svg icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,14 @@
+import './NavBar.css'
+import SwordSvg from './SwordSvg'
 function NavBar() {
     return (
         <nav>
             <ul>
+                <li>
+                    <a href="/" >
+                        <SwordSvg/>
+                    </a>
+                </li>
                 <li><a href="/">Home</a></li>
                 <li><a href="/blogs">Blogs</a></li>
             </ul>
