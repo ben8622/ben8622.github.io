a069ef6c68375cd0d70ab623c4905df913d2e055
ben8622 <benjaminjknight@gmail.com>
2025-11-24 16:23:44 -0600

    blog post and tags updates

---
+++ src/components/Tag.tsx
@@ -0,0 +1,11 @@
+import './Tag.css'
+
+function Tag({name}: {name: string}) {
+    return (
+        <div className="tag">
+            {name}
+        </div>
+    )
+}
+
+export default Tag
--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -2,15 +2,30 @@ import { useParams } from "react-router"
 import  {posts, postsRecord} from "../posts/postUtils"
 import Markdown from 'react-markdown'
 import NotFound from "./NotFound"
+import Tag from "../components/Tag"
+import './BlogPost.css'
 
 function BlogPost() {
     const {slug = ""} = useParams()
-    console.log("loaded posts:", posts)
+    const post = postsRecord[slug]
 
-    return postsRecord[slug] ? (
-        <Markdown>
-            {postsRecord[slug].content}
-        </Markdown>
+    return post ? (
+        <div className="blog-post-container">
+            <div className="blog-post-header">
+                <h1>{post.metadata.title}</h1>
+                { post.metadata.author && <p>post.metadata.author</p>}
+                { post.metadata.publishedOn && <p>Published on {post.metadata.publishedOn}</p>}
+                { post.metadata.modifiedOn && <p>Modified on {post.metadata.modifiedOn}</p>}
+                { post.metadata.tags && post.metadata.tags.length > 0 && (
+                    <div className="tags-container">
+                        {post.metadata.tags.map(tag => <Tag name={tag} key={tag}/> )}
+                    </div>
+                )}
+            </div>
+            <Markdown>
+                {post.content}
+            </Markdown>
+        </div>
     ) : (
         <NotFound />
     )
